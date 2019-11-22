637369c84541d318b89560fe5cf5bd812a2a0c75
José Castañeda <josecasta.r@gmail.com>
2019-11-22 10:10:52 -0600

    instalacion de libreria moment y coneccion a mongodb atlas online.

--- src/models/User.js
+++ src/models/User.js
@@ -3,12 +3,17 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema({
   username: String,
-  userAvatar: String,
   email: String,
   password: String,
-  signupDate: { type: Date, default: Date.now() }
+  role: String,
+  userAvatar: {
+    type: String,
+    default:
+      "https://cdn0.iconfinder.com/data/icons/hexagon-business-flat-part-2/218/Working_women-512.png"
+  }
 });
 
+userSchema.set("timestamps", true);
 userSchema.methods.encriptarClave = async password => {
   const salt = await bcrypt.genSalt(10);
   return bcrypt.hash(password, salt);
